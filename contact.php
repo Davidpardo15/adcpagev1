7102dc5b09cab8ad6a394f23dbf1b98df94c7e48
Nicolas Pardo <nicolas.pardo@andessy.com>
2023-02-23 16:20:42 -0500

    contact us

--- contact.php
+++ contact.php
@@ -12,29 +12,7 @@ if (!defined("PHP_EOL")) define("PHP_EOL", "\r\n");
 $email    = $_POST['email'];
 $name     = $_POST['name'];
 $subject  = $_POST['subject'];
-$message = $_POST['message'];
-
-
-if(trim($name) == '') {
-	echo '<div class="error_message">Attention! You must enter your name.</div>';
-	exit();
-}  else if(trim($email) == '') {
-	echo '<div class="error_message">Attention! Please enter a valid email address.</div>';
-	exit();
-} else if(!isEmail($email)) {
-	echo '<div class="error_message">Attention! You have enter an invalid e-mail address, try again.</div>';
-	exit();
-}
-
-if(trim($message) == '') {
-	echo '<div class="error_message">Attention! Please enter your message.</div>';
-	exit();
-}
-
-if(get_magic_quotes_gpc()) {
-	$message = stripslashes($message);
-}
-
+$mess = $_POST['message'];
 
 // Configuration option.
 // Enter the email address that you want to emails to be sent to.
@@ -42,6 +20,7 @@ if(get_magic_quotes_gpc()) {
 
 //$address = "example@themeforest.net";
 $address = "nicolas.pardo@andessy.com";
+$to = $email;
 
 
 // Configuration option.
@@ -49,7 +28,7 @@ $address = "nicolas.pardo@andessy.com";
 
 // Example, $e_subject = '$name . ' has contacted you via Your Website.';
 
-$e_subject = 'You\'ve been contacted by ' . $first_name . '.';
+$e_subject = 'You\'ve been contacted by ' . $name . '.';
 
 
 // Configuration option.
@@ -57,29 +36,140 @@ $e_subject = 'You\'ve been contacted by ' . $first_name . '.';
 // Developers, you may wish to add more fields to the form, in which case you must be sure to add them here.
 
 $e_body = "You have been contacted by $name." . PHP_EOL . PHP_EOL;
-$e_content = "\"$message\"" . PHP_EOL . PHP_EOL;
+$e_content = "\"$mess\"" . PHP_EOL . PHP_EOL;
 $e_reply = "You can contact $name via email.";
 
-$msg = wordwrap( $e_body . $e_content . $e_reply, 70 );
+$message  = '
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta name="x-apple-disable-message-reformatting">
+    <title></title>
+    <style>
+      table, td, div, h1, p {font-family: Arial, sans-serif;}
+    </style>
+  </head>
+  <body style="margin:0;padding:0;">
+    <table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;background:#ffffff;">
+      <tr>
+        <td align="center" style="padding:0;">
+          <table role="presentation" style="width:602px;border-collapse:collapse;border:1px solid #cccccc;border-spacing:0;text-align:left;">
+            <tr>
+              <td align="center" style="padding:40px 0 30px 0;background:#28a5df;">
+                <img src="https://s3.us-east-2.amazonaws.com/andessy.com/assets/img/navbar-logo.png" alt="" width="300" style="height:auto;display:block;" />
+              </td>
+            </tr>
+            <tr>
+              <td style="padding:36px 30px 42px 30px;">
+                <table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;">
+                  <tr>
+                    <td style="padding:0 0 36px 0;color:#153643;">
+                      <h1 style="font-size:24px;margin:0 0 20px 0;font-family:Arial,sans-serif; text-align: center">Un usuario quiere contactarse con Aircraft Digital Company</h1>
+                      <p style="margin:0 0 12px 0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;">'.$name.' ha realizado una solicitud de contacto con Aircraft Digital Company, desde el correo '.$email.': <br><br><br> '.$name.' send a message in Aircraft Digital Company:<br><br>'.$mess.'</p>
+                    </td>
+                  </tr>
+                </table>
+              </td>
+            </tr>
+            <tr>
+              <td style="padding:30px;background:#28a5df;">
+                <table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;font-size:9px;font-family:Arial,sans-serif;">
+                  <tr>
+                    <td style="padding:0;width:50%;" align="left">
+                    <p style="margin:0;font-size:14px;line-height:16px;font-family:Arial,sans-serif;color:#ffffff;">
+						Aircraft Digital Company SAS <br><br><br>
+						Carrera 19b #84-17<br>
+						Torre Over<br>
+						Bogotá D.C, Colombia<br>
+						+57 (601)744-4013<br>
+						info@aircraft-digital.com<br>
+				   	</p>
+                    </td>
+                  </tr>
+                </table>
+              </td>
+            </tr>
+          </table>
+        </td>
+      </tr>
+    </table>
+  </body>
+</html>
+';
 
-$headers = "From: $email" . PHP_EOL;
-$headers .= "Reply-To: $email" . PHP_EOL;
-$headers .= "MIME-Version: 1.0" . PHP_EOL;
-$headers .= "Content-type: text/plain; charset=utf-8" . PHP_EOL;
-$headers .= "Content-Transfer-Encoding: quoted-printable" . PHP_EOL;
+$message_to = '
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta name="x-apple-disable-message-reformatting">
+    <title></title>
+    <style>
+      table, td, div, h1, p {font-family: Arial, sans-serif;}
+    </style>
+  </head>
+  <body style="margin:0;padding:0;">
+    <table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;background:#ffffff;">
+      <tr>
+        <td align="center" style="padding:0;">
+          <table role="presentation" style="width:602px;border-collapse:collapse;border:1px solid #cccccc;border-spacing:0;text-align:left;">
+            <tr>
+              <td align="center" style="padding:40px 0 30px 0;background:#28a5df;">
+                <img src="https://s3.us-east-2.amazonaws.com/andessy.com/assets/img/navbar-logo.png" alt="" width="300" style="height:auto;display:block;" />
+              </td>
+            </tr>
+            <tr>
+              <td style="padding:36px 30px 42px 30px;">
+                <table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;">
+                  <tr>
+                    <td style="padding:0 0 36px 0;color:#153643;">
+                      <h1 style="font-size:24px;margin:0 0 20px 0;font-family:Arial,sans-serif; text-align: center">Realizaste una solicitud de contacto con Aircraft Digital Company</h1>
+                      <p style="margin:0 0 12px 0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;">'.$name.' has realizado una solicitud de contacto con Aircraft Digital Company. Nos estaremos comunicando muy pronto con tigo.</p><br><br>
+					  <p style="margin:0 0 12px 0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;">'.$name. ' Aircraft Digital te desa un exelente dia </p>
+                    </td>
+                  </tr>
+                </table>
+              </td>
+            </tr>
+            <tr>
+              <td style="padding:30px;background:#28a5df;">
+                <table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;font-size:9px;font-family:Arial,sans-serif;">
+                  <tr>
+                    <td style="padding:0;width:50%;" align="left">
+                    <p style="margin:0;font-size:14px;line-height:16px;font-family:Arial,sans-serif;color:#ffffff;"><a href="aircraft-digital.com">
+                    	Aircraft Digital Company SAS </a><br><br><br>
+						Carrera 19b #84-17<br>
+						Torre Over<br>
+						Bogotá D.C, Colombia<br>
+						+57 (601)744-4013<br>
+						info@aircraft-digital.com<br>
+				   	</p>
+                      </p>
+                    </td>
+                  </tr>
+                </table>
+              </td>
+            </tr>
+          </table>
+        </td>
+      </tr>
+    </table>
+  </body>
+</html>
+';
 
-if(mail($address, $e_subject, $msg, $headers)) {
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+
+mail($to, $e_subject, $message_to, $headers);
+if(mail($address, $e_subject, $message, $headers)) {
 
 	// Email has sent successfully, echo a success page.
 
-	echo "<fieldset>";
-	echo "<div id='success_page'>";
-	echo "<h1>Email Sent Successfully.</h1>";
-	echo "<p>Thank you <strong>$name</strong>, your message has been submitted to us.</p>";
-	echo "</div>";
-	echo "</fieldset>";
 
-    echo "<script type='text/javascript'>alert('Tu mensaje ha sido enviado exitosamente');</script>";
+	echo "<script>alert('Mensaje enviado con exito')</script>";
+	echo "<script>location.href='../contact.html'</script>";
 
 } else {
 
@@ -87,3 +177,5 @@ if(mail($address, $e_subject, $msg, $headers)) {
 
 }
 ?>
+
+
