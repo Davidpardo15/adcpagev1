8b6aec52d17ab8634a0938296f14574f11627d65
Nicolas Pardo <nicolas.pardo@andessy.com>
2023-02-23 15:31:16 -0500

    contadores

--- js/js.js
+++ js/js.js
@@ -26,3 +26,16 @@ window.addEventListener('load', function(e) {
 		}, 3500);
 	}, 5500);
 });
+
+
+$('.counter-count').each(function () {
+	$(this).prop('Counter',0).animate({
+		Counter: $(this).text()
+	}, {
+		duration: 5000,
+		easing: 'swing',
+		step: function (now) {
+			$(this).text(Math.ceil(now));
+		}
+	});
+});
